a716b126e6f30da5be35b382636e32a4f4f40bd7
Santiago Hewett <santiago.hewett@stu.ocsb.ca>
2023-12-19 12:55:02 -0500

    Done CPT-01

--- code.py
+++ code.py
@@ -1,1 +1,22 @@
-print("Hello, Santiago!")
+#!/usr/bin/env python3
+
+# Created by: Santiago Hewett
+# Created on: Dec 19
+# This program is the Space Sliens program on the PyBadge
+
+import stage
+import ugame
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, Santiago!")
+
+    # repeat forever, game loop
+    while True:
+        pass # placeholder
+
+if __name__ == "__main__":
+    game_scene
